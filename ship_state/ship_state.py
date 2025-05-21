8fa27d2fce780862de4688e98b83de6749c2186b
L. Bálint <lakatos.balint@techtra.hu>
2025-05-21 17:47:52 +0200

    VALIDATE basic shipState methods

--- ship_state/ship_state.py
+++ ship_state/ship_state.py
@@ -1,6 +1,7 @@
 # ship_state/ship_state.py
 """Ide kellenek a dinamikus tulajdonságok."""
 
+import numpy as np
 from gps_coordinate import ObjectiveCoordinate, ShipPosition
 
 
@@ -26,6 +27,35 @@ class ShipState:
         self.v: float  # sway speed
         self.r: float  # yaw rate
 
+    def eta(self) -> np.ndarray:
+        """Return position and heading vector in Earth-fixed frame."""
+        return np.array([self.x, self.y, self.psi])
+
+    def nu(self) -> np.ndarray:
+        """Return velocity vector in Body-fixed frame."""
+        return np.array([self.u, self.v, self.r])
+
+    def x_state(self) -> np.ndarray:
+        """Full state vector x = [eta.T, nu.T].T"""
+        return np.concatenate((self.eta(), self.nu()))
+
+    def eta_dot(self) -> np.ndarray:
+        """Calculate Earth-frame velocity \u03b7_dot = R(psi) * nu (Equation 2)."""
+        R = self.rotation_matrix()
+        nu = self.nu()
+        return R @ nu
+
+    def rotation_matrix(self) -> np.ndarray:
+        """Return the rotation matrix R(psi) from body to Earth frame (Equation 3)."""
+
+        c = np.cos(self.psi)
+        s = np.sin(self.psi)
+
+        return np.array([
+            [c, -s, 0],
+            [s,  c, 0],
+            [0,  0, 1]
+        ])
 
         # TODO
         ...
