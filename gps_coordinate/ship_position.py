2512f57a37afd46fab2a7155574cb231a726247d
L. Bálint <lakatos.balint@techtra.hu>
2025-05-22 14:09:34 +0200

    heading, Xb, Yb props | ned_offset func

--- gps_coordinate/ship_position.py
+++ gps_coordinate/ship_position.py
@@ -1,5 +1,6 @@
 # ship_position.py
 
+from math import sin, cos
 import os
 from threading import Lock
 from loguru import logger
@@ -24,7 +25,7 @@ class ShipPosition(GPSPoint):
     _singleton_lock = Lock()
     _lock = Lock()
 
-    def __new__(cls, latitude=0.0, longitude=0.0, geofence = None):
+    def __new__(cls, latitude=0.0, longitude=0.0, geofence=None):
         with cls._singleton_lock:
             if cls._instance is None:
                 cls._instance = super().__new__(cls)
@@ -40,6 +41,40 @@ class ShipPosition(GPSPoint):
             # A 'getattr' igenis eléri, csak ezt a linter nem tudja szegény
             self.__initialized = True
             self.geofence = geofence
+
+    # region properties
+
+    @property
+    def heading_psi(self) -> float:
+        """Ship heading in radians"""
+        with self._lock:
+            return getattr(self, "_heading_psi", 0.0)
+
+    @property
+    def Xb(self) -> float:
+        north, east = self.ned_offset()
+        psi = self.heading_psi
+        Xb = cos(psi) * north + sin(psi) * east
+        return Xb
+
+    @property
+    def Yb(self) -> float:
+        north, east = self.ned_offset()
+        psi = self.heading_psi
+        Yb = -sin(psi) * north + cos(psi) * east
+        return Yb
+
+    # endregion
+
+    def ned_offset(self, reference_point: GPSPoint) -> tuple[float, float]:
+        """
+        NED offset relative to a fixed Earth frame origin.
+        For body-frame projection, the origin is the initial position.
+        """
+        if reference_point is None:
+            logger.warning("Origin not set for body-fixed transformation. Using self.")
+            reference_point = self
+        return self.ned_offset_from(reference_point)
 
     def update_position(self, new_objective: ObjectiveCoordinate) -> bool:
 
