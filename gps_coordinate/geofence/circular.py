42e47e9da39c1589d6b0fd42033c1ce30f1b5a19
L. Bálint <lakatos.balint@techtra.hu>
2025-05-23 15:14:25 +0200

    guard msg for circularFence

--- gps_coordinate/geofence/circular.py
+++ gps_coordinate/geofence/circular.py
@@ -1,5 +1,6 @@
 from .base import Geofence
 from ..base import GPSPoint
+from loguru import logger
 
 
 class CircularGeofence(Geofence):
@@ -8,4 +9,11 @@ class CircularGeofence(Geofence):
         self.radius = radius_m
 
     def contains(self, point: GPSPoint) -> bool:
-        return self.center.haversine_distance(point) <= self.radius
+        _dist = self.center.haversine_distance(point)
+        _contains = _dist <= self.radius
+
+        if not _contains:
+            logger.error(f"Error at CircularGeofence: R({self.radius}) < Dist({_dist}). \
+                         Points: [{self.center}] [{point}]")
+
+        return _contains
