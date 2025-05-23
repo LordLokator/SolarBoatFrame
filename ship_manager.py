0a44cdb89b255f9e695e3db4cddab292e2a6f4c5
L. Bálint <lakatos.balint@techtra.hu>
2025-05-23 13:42:34 +0200

    ShipManager now has better attributes, geofence. | get_next_objective_coo doesn't throw exception on empty task list now

--- ship_manager.py
+++ ship_manager.py
@@ -1,4 +1,8 @@
-from gps_coordinate.objective import ObjectiveCoordinate
+from loguru import logger
+from gps_coordinate import CircularGeofence
+from gps_coordinate import ObjectiveCoordinate
+from gps_coordinate import ShipPosition
+from gps_coordinate import GPSPoint
 from ship_state.ship_properties import BlueLadyShipProperties
 from ship_state.ship_state import ShipState
 
@@ -14,13 +18,21 @@ class ShipManager:
 
     def __init__(self):
         self.ship_properties = BlueLadyShipProperties()
-        self.ship_state = ShipState()
+
+        _geofence_center = GPSPoint(47.47, 19.03)  # ~ Kelenföld
+        geofence_huge = CircularGeofence(center=_geofence_center, radius_m=30_000)  # 30km
+        starting_position = ShipPosition(geofence=geofence_huge)
+        self.ship_state = ShipState(starting_position)
 
         # TODO
         ...
 
     def step(self) -> None:
-        ...
+        print(self.ship_state.position)
 
     def get_next_objective_coo(self) -> ObjectiveCoordinate:
+        if len(self.ship_state.route) < 1:
+            logger.warning(f"ShipState has no more objectives in route list!")
+            return None
+
         return self.ship_state.route[0]
