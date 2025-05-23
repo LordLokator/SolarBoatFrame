a4789054652d47318c1df7755e174a0a1e6a0d30
L. Bálint <lakatos.balint@techtra.hu>
2025-05-23 13:34:50 +0200

    added geofences to __init__

--- gps_coordinate/__init__.py
+++ gps_coordinate/__init__.py
@@ -1,8 +1,10 @@
 # __init__.py
 
+from .geofence.circular import CircularGeofence
+from .geofence.polygonal import PolygonalGeofence
 from .base import GPSPoint
 from .ship_position import ShipPosition
 from .buoy import BuoyPosition
 from .objective import ObjectiveCoordinate
 
-__all__ = ["GPSPoint", "ShipPosition", "BuoyPosition", "ObjectiveCoordinate"]
+__all__ = ["GPSPoint", "ShipPosition", "BuoyPosition", "ObjectiveCoordinate", "CircularGeofence", "PolygonalGeofence"]
