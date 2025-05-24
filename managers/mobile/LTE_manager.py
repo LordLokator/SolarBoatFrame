82f15c9b491bd1cefc8eb129e5864f0b7f1d39a0
LordLokator <lakybalint@gmail.com>
2025-05-24 17:35:02 +0200

    use helper fnc to get own IP |format

--- managers/mobile/LTE_manager.py
+++ managers/mobile/LTE_manager.py
@@ -1,12 +1,17 @@
 # Placeholder class.
 
+from loguru import logger
+from .net_helper import get_local_ip
+
+
 class LTEManager:
     def __init__(self):
+        self.ip_addr = get_local_ip()
+
         raise NotImplementedError()
 
     def connect(self) -> None:
         # Establish PPP or check SIM/registration
-
         raise NotImplementedError()
 
     def get_signal_quality(self) -> float:
