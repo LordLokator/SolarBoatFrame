7805a8185ae61dc725d2fb32c9ae676d199fba0b
L. Bálint <lakatos.balint@techtra.hu>
2025-05-26 10:26:47 +0200

    LTE test skeleton

--- tests/test_managers/test_4g.py
+++ tests/test_managers/test_4g.py
@@ -1,5 +1,20 @@
-# tests/test_4g.py
+# tests/test_managers/test_4g.py
 
-# TODO: Test 4g network.
-# Might not need 4g from the frameowrk!
-# Can't hurt to at least test any 4g module we try.
+import unittest
+
+from managers import LTEManager
+
+
+class TestEquationShipState(unittest.TestCase):
+
+    def test_constructor(self):
+        lte_manager = LTEManager()
+        self.assertTrue(lte_manager)
+
+    def test_connect_success(self):
+        ...
+        # TODO test cases once LTE implemented
+
+
+if __name__ == '__main__':
+    unittest.main()
