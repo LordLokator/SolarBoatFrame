a7aad2a0b3828bb00c3ffa0076fa3f2d8c1d5806
L. Bálint <lakatos.balint@techtra.hu>
2025-05-26 14:24:05 +0200

    __all__ formatting

--- ship/__init__.py
+++ ship/__init__.py
@@ -5,4 +5,10 @@ from .ship_properties import BlueLadyShipProperties, LanaShipProperties
 from .ship_state import ShipState
 from .task import ShipTaskManager
 
-__all__ = ['ShipPosition', 'BlueLadyShipProperties', 'LanaShipProperties', 'ShipState', 'ShipTaskManager']
+__all__ = [
+    'ShipPosition',
+    'BlueLadyShipProperties',
+    'LanaShipProperties',
+    'ShipState',
+    'ShipTaskManager'
+]
