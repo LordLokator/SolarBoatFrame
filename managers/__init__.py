7541086e00710a28af977a4f34975ed3891bc224
L. Bálint <lakatos.balint@techtra.hu>
2025-05-27 17:07:41 +0200

    added mock GPS to module init

--- managers/__init__.py
+++ managers/__init__.py
@@ -2,6 +2,7 @@
 
 from .can_bus.can_manager import CANManager
 from .gps_sensor.gps_manager import GPSManager
+from .gps_sensor.mock_gps_manager import MockGPSManager
 from .mobile.LTE_manager import LTEManager
 
-__all__ = ["CANManager", "GPSManager", "LTEManager"]
+__all__ = ["CANManager", "GPSManager", "MockGPSManager", "LTEManager"]
