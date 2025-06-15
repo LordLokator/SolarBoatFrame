112255925a538101aa90dca8d5cf25463a0432ce
Bálint Lakatos <lakatos.balint@techtra.hu>
2025-06-15 08:49:07 +0200

    MockGPS also returns heading

--- managers/gps_sensor/mock_gps_manager.py
+++ managers/gps_sensor/mock_gps_manager.py
@@ -1,11 +1,13 @@
 class MockGPSManager:
-    def __init__(self, lat, lon):
+    def __init__(self, lat, lon, heading):
         self._lat = lat
         self._lon = lon
+        self._heading = heading
 
     def get_live_location(self):
-        return self._lat, self._lon
+        return self._lat, self._lon, self._heading
 
-    def set_mock_location(self, lat, lon):
+    def set_mock_location(self, lat, lon, heading):
         self._lat = lat
         self._lon = lon
+        self._heading = heading
